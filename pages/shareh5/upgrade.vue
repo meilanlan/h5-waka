e747585f05c8a31e30dd2b15afe5b52c24294111
meilanlan <50614184+meilanlan@users.noreply.github.com>
2026-06-02 21:57:04 +0800

    新增升级h5

---
+++ pages/shareh5/upgrade.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="wrapper">
+    <myCustomNavbar title="" @backPage="backPage"></myCustomNavbar>
+    <image src="/static/image/upgrade/01.jpg" mode="widthFix"></image>
+    <image src="/static/image/upgrade/02.jpg" mode="widthFix"></image>
+    <image src="/static/image/upgrade/03.jpg" mode="widthFix"></image>
+    <image src="/static/image/upgrade/04.jpg" mode="widthFix"></image>
+  </view>
+  
+</template>
+
+<script setup>
+  import { ref, onMounted, nextTick } from 'vue'
+  import myCustomNavbar from '../../components/myCustomNavbar.vue'
+  
+  function backPage() {
+    window.client.closeWebview()
+  }
+  
+  onMounted(() => {
+    const appPage = document.getElementById('app')
+    if (appPage) {
+      appPage.style.paddingTop = 0
+    }
+  })
+</script>
+
+<style scoped>
+  image {
+    display: block;
+    width: 100vw;
+  }
+</style>
